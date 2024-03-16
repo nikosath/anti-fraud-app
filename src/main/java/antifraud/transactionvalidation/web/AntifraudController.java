036ba1f05a3508b6c461f8d70da9a23c8c6272ac
Nikos Athanasakis <emidekol@gmail.com>
2024-03-16 16:33:56 +0200

    Rename field

--- src/main/java/antifraud/transactionvalidation/web/AntifraudController.java
+++ src/main/java/antifraud/transactionvalidation/web/AntifraudController.java
@@ -44,22 +44,22 @@ public class AntifraudController {
     }
 
     /**
-     * @param amount              transaction amount
-     * @param ip                  ip address of the transaction initiator
-     * @param number              credit card of the transaction initiator
+     * @param amount transaction amount
+     * @param ip     ip address of the transaction initiator
+     * @param number credit card of the transaction initiator
      * @param region
-     * @param transactionDateTime
+     * @param date   transaction date-time
      */
     public record ValidateTransactionRequest(
             @Min(1) long amount,
             @NotBlank @Pattern(regexp = Regexp.IP_ADDRESS) String ip,
             @NotBlank @CreditCardNumber String number, antifraud.transactionvalidation.RegionCode region,
-            java.time.LocalDateTime transactionDateTime) {
+            java.time.LocalDateTime date) {
     }
 
     public record ValidateTransactionResponse(TransactionStatusEnum result, String info) {
         ValidateTransactionResponse(TransactionValidation.TransactionApprovalStatus status) {
-           this(status.transactionStatus(), status.statusJustification());
+            this(status.transactionStatus(), status.statusJustification());
         }
     }
 
